7a52a3635b04449898d7122e8aa8239a413770cd
Thiago Neves <thiago-snf@hotmail.com>
2022-03-07 22:08:54 -0300

    beecrowd

---
+++ 1 - Iniciante/BEE1011.py
@@ -0,0 +1,3 @@
+R = float(input())
+
+print(f"VOLUME = {4/3 * 3.14159 * R**(3) :.3f}")
---
+++ 1 - Iniciante/BEE1012.py
@@ -0,0 +1,7 @@
+A, B, C = [float(x) for x in input().split()]
+
+print(f"TRIANGULO: {A * C / 2:.3f}")
+print(f"CIRCULO: {3.14159 * C**2:.3f}")
+print(f"TRAPEZIO: {(A + B) * C / 2:.3f}")
+print(f"QUADRADO: {B**2:.3f}")
+print(f"RETANGULO: {A * B:.3f}")
